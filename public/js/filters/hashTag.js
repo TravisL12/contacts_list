0bc8371975569ca35bf8fcd670b428bc0b28ca01
Travis Lawrence <travis.lawrence12@gmail.com>
2016-10-11 22:55:30 -0700

    Remove old css, update filter

--- public/js/filters/hashTag.js
+++ public/js/filters/hashTag.js
@@ -2,6 +2,9 @@
 
 angular.module('honeyBookApp').filter('hashTag', [function () {
     return function (name) {
-        return '@' + name.replace(/[\s-\.*?!{}\[\]()]/g,'');
+        if (name) {
+            return '@' + name.replace(/[\s-\.*?!{}\[\]()]/g,'');
+        }
+        return;
     }
 }])
