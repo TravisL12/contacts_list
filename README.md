22c1b6a6705b458e8c259be4a8ef7899a84fddea
Travis Lawrence <travis.lawrence12@gmail.com>
2016-10-13 07:45:49 -0700

    ngAnimate for search filtering

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## This is a simple AngularJS skeleton that has the following files included:
-- AngularJS 1.4.5 (CDN)
-- jQuery 1.9 (CDN)
-- Twitter Bootstrap 3.3.6
+# Honeybook Demonstration Page
+### By Travis Lawrence (October 2016)
+
+- A little take home coding exercise for Honeybook
--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,41 @@
 'use strict';
 
-angular.module('honeyBookApp', ['ui.router']).config(function ($urlRouterProvider, $locationProvider, $stateProvider) {
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise('/');
+angular.module('honeyBookApp', ['ui.router', 'ngAnimate']).config(['$urlRouterProvider', '$locationProvider', '$stateProvider', 
+    function ($urlRouterProvider, $locationProvider, $stateProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise('/');
 
-    $stateProvider
-    .state('honeybook', {
-        url: '',
-        abstract: true,
-        views: {
-            'header': {
-                templateUrl: '/public/views/header.html',
-                controllerAs: 'vmHeader',
-                controller: ['$scope', function ($scope) {
-                    this.sendSearch = function () {
-                        $scope.$emit('searchContacts', this.search.keyword);
-                    };
-                }]
+        $stateProvider
+        .state('honeybook', {
+            url: '',
+            abstract: true,
+            views: {
+                'header': {
+                    templateUrl: '/public/views/header.html',
+                    controllerAs: 'vmHeader',
+                    controller: ['$scope', function ($scope) {
+                        this.sendSearch = function () {
+                            $scope.$emit('searchContacts', this.search.keyword);
+                        };
+                    }]
+                }
+            },
+            resolve: {
+                HoneybookContacts: function ($http, Addressbook) {
+                    return $http.get('https://candidate-test.herokuapp.com/contacts').then(function (response) {
+                        Addressbook.contacts = response.data;
+                    });
+                }
             }
-        },
-        resolve: {
-            HoneybookContacts: function ($http, Addressbook) {
-                return $http.get('https://candidate-test.herokuapp.com/contacts').then(function (response) {
-                    Addressbook.contacts = response.data;
-                });
+        })
+        .state('honeybook.contacts', {
+            url: '/',
+            views: {
+                'main@': {
+                    templateUrl: '/public/views/contacts.html',
+                    controller: 'ContactsController as vmContacts'
+                }
             }
-        }
-    })
-    .state('honeybook.contacts', {
-        url: '/',
-        views: {
-            'main@': {
-                templateUrl: '/public/views/contacts.html',
-                controller: 'ContactsController as vmContacts'
-            }
-        }
-    })
-}).run();
+        })
+    }
+]);
